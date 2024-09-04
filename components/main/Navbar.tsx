d2b940fbc8dac52997648f0a0683b2702a899dc4
ChelseaMaramot <gracechelsea76@yahoo.com>
2024-09-04 17:43:25 -0400

    fixed nav bar spacing

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -4,13 +4,13 @@ const Navbar = () => {
   return (
     <div className='w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417 backdrop-blur-md z-50 px-10]'>
 
-        <div className='w-full h-full flex flex-row items-center justify-end px-[75px]'>
+        <div className='w-full h-full flex flex-row items-center justify-between px-[60px]'>
             
-            <div className='text-gray-200'>
+            <div className='float-left text-gray-200 text-xl tracking-[0.3em] '>
                 <a>ROYA MOTAZEDIAN</a>
             </div>
         
-            <div className='flex flex-row gap-10 border-[#7042f861] rounded-full text-gray-200'>
+            <div className='flex flex-row gap-10 float-right border-[#7042f861] rounded-full text-gray-200'>
                 <a href="#Work" className='cursor-pointer'>Work</a>
                 <a href="#About" className='cursor-pointer'>About</a>
             </div>
