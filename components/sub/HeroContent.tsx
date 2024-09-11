7e01e59e73df6307cd2504c549726ce642ac4cef
ChelseaMaramot <gracechelsea76@yahoo.com>
2024-09-11 12:25:41 -0400

    added button links

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -9,6 +9,7 @@ import { useTypewriter, Cursor } from 'react-simple-typewriter';
 const HeroContent = () => {
 
     const name = 'Roya';
+    const email = "roya9mota@gmail.com";
 
     const [text] = useTypewriter({
         words: [name, 'Writer', 'Poet', 'Storyteller'],
@@ -21,8 +22,12 @@ const HeroContent = () => {
         I&apos;m&nbsp;<span style={{ color: '#25EEFA', fontWeight: 'bold' }}>{text}</span><span><Cursor></Cursor></span>
       </div>
       <div className='flex flex-row justify-center mt-6 space-x-3 z-40'> 
-        <button type="button" className='text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'>Connect with me</button>
-        <button type="button" className='text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'>My resume</button>
+        <a href={`mailto:${email}`}> 
+          <button type="button" className='text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'>Connect with me</button>
+        </a>
+        <a href="resume.pdf" target='_blank'>
+          <button type="button" className='text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700'>My resume</button>
+        </a>
       </div>
    </div>
   )
