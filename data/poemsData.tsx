2b4c20b2a671fa2ad2516ec3463f0dab78f16bd6
ChelseaMaramot <gracechelsea76@yahoo.com>
2024-09-13 19:24:27 -0400

    adding poem

--- data/poemsData.tsx
+++ data/poemsData.tsx
@@ -4,17 +4,79 @@ export const poems = [
 { 
     src: '/PoemImage/poem1Image.jpeg',
     title: 'poem1',
-    content: 'This is the content of poem 1.',
+    content: `she pulls me in so close her ear cold on my cheek rocking me from side to side
+though i'm not a baby in a mother's arms i am a warrior saying my final goodbyes melting away the layers adulthood has formed becoming a child again as you tell me to have a safe flight
+and when i picture myself on a plane i also picture myself coming back to you i'll leave to grow into someone who is worthy of your love
+and i'll return to give it all back to you
+but for now it'll just be me myself and i
+carry your love on my back like a turtle make it my home
+crawl in no matter where i am the desert winds could dry me i could drown in an ocean the snow could freeze me i could suffocate under the rain but this shell will always stay the same the parts of me that don't change are the pieces of yourself that you gave to me
+i wove them into an umbrella the hood of a sweater two hands over a head
+but still for now it'll just be me myself and i
+as i learn to love myself in your absence space i thought i wanted not sure what i needed
+retract the hand i used to reach out to you i'm a warrior now
+fighting all these battles on my own but you'll always be there
+the edges of this body that you sanded
+'till it sparked
+'till it stabbed
+are the parts that save me when i'm under attack
+so you hug me and tell me to come back but before i knew what home was i already knew it would call me back to itself when i was gone too long
+and i wouldn't even wait for that call to come back
+this body is a boomerang
+can throw me far but i'll always return hang my head low and face it alone until i finally come back to you
+`,
   },
   {
     src: '/PoemImage/poem2Image.jpeg',
     title: 'poem2',
-    content: 'This is the content of poem 2.',
+    content: `I hate the phrase “you’re so lucky “
+as if you didn't grit your teeth and carry so much on your back
+to get here as if it wasn't hitting the soil
+a hundred times over
+and over and over
+that got you what you wanted that helped your tree bear fruits
+your blood sweat tears
+the water
+the fire of your passion the sun
+and when the wind got knocked out of you, you fed your tree carbon dioxide every breath out was to
+create the air you breathe in for your own lungs
+'youre so lucky'
+and yeah the stars did align but they wouldnt have if you didnt rotate the entire universe on your own
+'youre so lucky' and i know you're not
+ you're so deserving you deserve it you worked for it prayed and begged for it
+and even if it seems like it was handed to you dont forget everything that came before take it and know this wasnt luck
+this was the smallest thing the universe could do for you
+in return for you lighting all the stars in the sky ablaze
+with your passion
+`,
   },
   {
     src: '/PoemImage/poem3Image.jpeg',
     title: 'poem3',
-    content: 'This is the content of poem 3.',
+    content: `everything i’ve craved 
+but never had was there 
+nutella
+ mariokart 
+a pet cat 
+a wii 
+wii sports 
+wii sports resort 
+monkey bars 
+a custom dollhouse
+ little pet shop dolls 
+bratz 
+monster high
+all the things in your dad's home office that stopped working so you and i would use them to play office and dammit why was i always the assistant while you were the manager 
+unlimited cinnamon gum packs
+ family 
+a big tv
+food that actually tasted good because my mom was always too busy and stressed to cook (i" never blame her though) so anything at yours was better
+so as they say
+'make yourself at home' i did that and got a little too comfortable ate too many of ur mom's dinners
+ i'll never taste that spaghetti or tomato cheese panini ever again
+don't blame you for not talking to me anymore 
+but ill always remember ur mom's house as more home than my own house ever was
+`,
   },
   {
     src: '/PoemImage/poem4Image.jpeg',
