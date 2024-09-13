3e69beb882512e0e979059bd6be4203407e25c4d
ChelseaMaramot <gracechelsea76@yahoo.com>
2024-09-13 19:43:48 -0400

    modal body spacing

--- components/sub/WorkModal.tsx
+++ components/sub/WorkModal.tsx
@@ -16,10 +16,10 @@ interface WorkModalProps {
 export default function WorkModal({isModalOpen, onModalClose, poem}: WorkModalProps) {
   return (
     <div className="w-full h-full absolute top-0 backdrop-filter backdrop-blur-sm">
-        <Modal hideCloseButton={true} size="xl" isOpen={isModalOpen} onClose={onModalClose} className="flex flex-column items-center rounded mt-10 mx-10 pt-5 px-5 bg-[#25EEFA] h-auto min-h-[30vh] max-h-[80vh]">
+        <Modal hideCloseButton={true} size="xl" isOpen={isModalOpen} onClose={onModalClose} className="font-serif flex flex-column items-center rounded  mt-10 mx-10 pt-5 px-5 bg-white h-auto min-h-[30vh] max-h-[80vh]">
             <ModalContent className="text-black">
                 <ModalHeader className="flex flex-col gap-1 my-5 text-xl font-bold">{poem.title}</ModalHeader>
-                <ModalBody className="p-1 whitespace-pre-wrap overflow-y-auto">{poem.content}</ModalBody>
+                <ModalBody className="p-1 whitespace-pre-wrap overflow-y-auto leading-9">{poem.content}</ModalBody>
                 <ModalFooter className="m-auto py-5">
                     <Button className="bg-blue-500 hover: bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onPress={onModalClose}>
                     Close
